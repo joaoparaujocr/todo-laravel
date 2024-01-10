426914ef797ad7c5402ecd527678bc7d62d9ea0d
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-10 18:51:26 -0400

    create tables

---
+++ database/migrations/2024_01_10_223532_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->text('description')->nullable(false);
+            $table->dateTime('due_date')->nullable(false);
+            $table->foreignId('category_id')->constrained('categories', 'id');
+            $table->foreignId('user_id')->constrained('users', 'id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->dropForeign(['category_id', 'user_id']);
+        });
+        Schema::dropIfExists('tasks');
+    }
+};
