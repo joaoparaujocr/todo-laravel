3abac0c3e4ea7bd36eedb9ecd5b58ff59760229f
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-01-16 21:27:01 -0400

    adding: basic structure front

---
+++ resources/views/layout/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap" rel="stylesheet">
+    <title>TodoApp</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="sidebar">
+            sidebar
+        </div>
+        <div class="content">
+            <header>
+                <nav>
+                    <a class="btn btn-primary" href="#">Criar tarefa</a>
+                </nav>
+            </header>
+            <main>
+                <div class="graph">grafico</div>
+                <div class="list">list</div>
+            </main>
+        </div>
+    </div>
+</body>
+
+</html>
